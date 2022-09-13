4f4f75e9bae272638a2f7f9481d06bc921a63e83
Petzr <peterjansen812@gmail.com>
2022-09-13 14:51:40 +0200

    asyncBoek opdr verbeter het is nu denk wel klaar en afgerond

--- asyncBoek/Program.cs
+++ asyncBoek/Program.cs
@@ -2,48 +2,52 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace asyncBoek
+namespace AsyncBoek
 {
     class Boek
     {
         public string Titel { get; set; }
         public string Auteur { get; set; }
-        public async Task<float> AIScore() {
-            
-            // Deze 'berekening' is eigenlijk een ingewikkeld AI algoritme.
-            // Pas de volgende vier regels niet aan.
-            double ret = 1.0f;
-            for (int i = 0; i < 10_000_000; i++)
-                for (int j = 0; j < 10; j++)
-                    ret = (ret + Willekeurig.Random.NextDouble()) % 1.0;
-            return (float)ret;
-            
+        public float AIScore {
+            get {
+                // Deze 'berekening' is eigenlijk een ingewikkeld AI algoritme.
+                // Pas de volgende vier regels niet aan.
+                double ret = 1.0f;
+                for (int i = 0; i < 10000000; i++)
+                    for (int j = 0; j < 10; j++)
+                        ret = (ret + Willekeurig.Random.NextDouble()) % 1.0;
+                return (float)ret;
+            }
         }
     }
-
+    static class Willekeurig
+    {
+        public static Random Random = new Random();
+        public static async Task Vertraging(int MinAantalMs = 500, int MaxAantalMs = 1000)
+        {
+            await Task.Delay(Random.Next(MinAantalMs, MaxAantalMs));
+        }
+    }
     static class Database
     {
         private static List<Boek> lijst = new List<Boek>();
-        public static async Task VoegToe(Boek b)
+        public static async void VoegToe(Boek b)
         {
-            await Willekeurig.Pauzeer(1000); // INSERT INTO ...
+            await Willekeurig.Vertraging(); // INSERT INTO ...
             lijst.Add(b);
         }
         public static async Task<List<Boek>> HaalLijstOp()
         {
-            await Willekeurig.Pauzeer(1200); // SELECT * FROM ...
+            await Willekeurig.Vertraging(); // SELECT * FROM ...
             return lijst;
         }
-        public static async Task Logboek(string melding)
+        public static async void Logboek(string melding)
         {
-            await Willekeurig.Pauzeer(2000); // schrijf naar logbestand
+            await Willekeurig.Vertraging(); // schrijf naar logbestand
         }
     }
-
-    public class Program
+    class Program
     {
-        static bool Backupping = false;
-
         static async Task VoegBoekToe() {
             Console.WriteLine("Geef de titel op: ");
             var titel = Console.ReadLine();
@@ -52,7 +56,7 @@ namespace asyncBoek
             Database.VoegToe(new Boek {Titel = titel, Auteur = auteur});
             Database.Logboek("Er is een nieuw boek!");
             Console.WriteLine("De huidige lijst met boeken is: ");
-            foreach (var boek in Database.HaalLijstOp().Result) {
+            foreach (var boek in await Database.HaalLijstOp()) {
                 Console.WriteLine(boek.Titel);
             }
         }
@@ -60,37 +64,32 @@ namespace asyncBoek
             Console.WriteLine("Waar gaat het boek over?");
             var beschrijving = Console.ReadLine();
             Boek beste = null;
-            foreach (var boek in Database.HaalLijstOp().Result)
-            {
-                var boekScore = await boek.AIScore();
-                if (boekScore > beste?.AIScore().Result)
+            foreach (var boek in await Database.HaalLijstOp())
+                if (beste == null || boek.AIScore > beste.AIScore)
                     beste = boek;
-                Console.WriteLine("Het boek dat het beste overeenkomt met de beschrijving is: ");
-                Console.WriteLine(beste.Titel);
-            }
+            Console.WriteLine("Het boek dat het beste overeenkomt met de beschrijving is: ");
+            Console.WriteLine(beste.Titel);
         }
+        static bool Backupping = false;
         // "Backup" kan lang duren. We willen dat de gebruiker niet hoeft te wachten,
         // en direct daarna boeken kan toevoegen en zoeken.
         static async Task Backup() {
             if (Backupping)
                 return;
             Backupping = true;
-            await Willekeurig.Pauzeer(3500);
-            Console.WriteLine("Backup is klaar");
+            await Willekeurig.Vertraging(2000, 3000);
+            Console.WriteLine("Backup gemaakt...");           // zelf bij geschreven voor duidelijkheid
             Backupping = false;
         }
-
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
             Console.WriteLine("Welkom bij de boeken administratie!");
             string key = null;
             while (key != "q") {
-                Console.WriteLine("\n\nWat wil je doen?");
                 Console.WriteLine("+) Boek toevoegen");
                 Console.WriteLine("z) Boek zoeken");
                 Console.WriteLine("b) Backup maken van de boeken");
                 Console.WriteLine("q) Quit");
-
                 key = Console.ReadLine();
                 if (key == "+")
                     VoegBoekToe();
