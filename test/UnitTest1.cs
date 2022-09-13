727315b0746184815fbf94fb643b6c4dc71b2617
Petzr <peterjansen812@gmail.com>
2022-09-13 14:33:04 +0200

    debug

--- test/UnitTest1.cs
+++ test/UnitTest1.cs
@@ -1,15 +1,12 @@
+using Moq;
+using Xunit;
 namespace test;
 
-public class Tests
+public class UnitTest1
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
-
-    [Test]
+    [Fact]
     public void Test1()
     {
-        Assert.Pass();
+        Assert.Equal(1, 1);
     }
 }
